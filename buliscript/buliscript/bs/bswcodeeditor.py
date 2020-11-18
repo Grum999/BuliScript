23306eee29cf9be1c72080e7ffe350ab70310123
Grum999 <grum999@grum.fr>
2020-11-18 18:14:02 +0100

    Improve code editor
    Add functions:
    - comment/uncomment
    - indent/dedent
    - ctrl+wheel (zoom in/out)

--- buliscript/buliscript/bs/bswcodeeditor.py
+++ buliscript/buliscript/bs/bswcodeeditor.py
@@ -23,6 +23,8 @@
 # From Qt documentation example "Code Editor"
 #  https://doc.qt.io/qtforpython-5.12/overviews/qtwidgets-widgets-codeeditor-example.html
 
+from math import ceil
+import re
 
 from PyQt5.Qt import *
 from PyQt5.QtWidgets import (
@@ -33,6 +35,9 @@ from .bssyntaxhighlighter import BSSyntaxHighlighter
 from .bslanguagedef import (
         BSLanguageDef
     )
+
+from ..pktk.pktk import EInvalidType
+from ..pktk.pktk import EInvalidValue
 
 
 class BSColorProp:
@@ -47,8 +52,16 @@ class BSColorProp:
     def foreground(self):
         return self.__foreground
 
+
 class BSWCodeEditor(QPlainTextEdit):
     """Extended editor with syntax highlighting, autocompletion, line nubmer"""
+
+    KEY_INDENT = 'indent'
+    KEY_DEDENT = 'dedent'
+    KEY_TOGGLE_COMMENT = 'toggleComment'
+
+    CTRL_KEY_TRUE = True
+    CTRL_KEY_FALSE = False
 
     def __init__(self, parent=None):
         super(BSWCodeEditor, self).__init__(parent)
@@ -64,6 +77,20 @@ class BSWCodeEditor(QPlainTextEdit):
         # define editor properties
         self.__colorGutter=BSColorProp(QColor('#4c5363'), QColor('#282c34'))
         self.__colorHighlightedLine=BSColorProp(QColor('#000000'), QColor('#2d323c'))
+        self.__indentWidth=4
+
+        self.__shortCuts={
+            Qt.Key_Tab: {
+                    BSWCodeEditor.CTRL_KEY_FALSE: BSWCodeEditor.KEY_INDENT
+                },
+            Qt.Key_Backtab: {
+                    # SHIFT+TAB = BACKTAB
+                    BSWCodeEditor.CTRL_KEY_FALSE: BSWCodeEditor.KEY_DEDENT
+                },
+            Qt.Key_Slash: {
+                    BSWCodeEditor.CTRL_KEY_TRUE: BSWCodeEditor.KEY_TOGGLE_COMMENT
+                }
+        }
 
         font = QFont()
         font.setFamily("Monospace")
@@ -74,7 +101,6 @@ class BSWCodeEditor(QPlainTextEdit):
         palette = self.palette()
         palette.setColor(QPalette.Active, QPalette.Base, QColor('#282c34'))
         palette.setColor(QPalette.Inactive, QPalette.Base, QColor('#282c34'))
-
 
         self.__highlighter = BSSyntaxHighlighter(self.document(), BSLanguageDef())
 
@@ -130,6 +156,44 @@ class BSWCodeEditor(QPlainTextEdit):
         self.setExtraSelections(extraSelections)
 
 
+    def __isEmptyBlock(self, blockNumber):
+        """Check is line for current block is empty or not"""
+
+        # get block text
+        cursor = self.textCursor()
+        cursor.movePosition(QTextCursor.Start)
+        cursor.movePosition(QTextCursor.NextBlock, n=blockNumber)
+        cursor.movePosition(QTextCursor.StartOfLine)
+        cursor.movePosition(QTextCursor.EndOfLine, QTextCursor.KeepAnchor)
+        text = cursor.selectedText()
+        if text.strip() == "":
+            return True
+        else:
+            return False
+
+
+    def __calculateIndent(self, position):
+        """Calculate indent to apply according to current position"""
+        indentValue = ceil(position/self.__indentWidth)*self.__indentWidth - position
+        if indentValue == 0:
+            indentValue = self.__indentWidth
+        return indentValue
+
+
+    def __calculateDedent(self, position):
+        """calculate indent to apply according to current position"""
+        if position > 0:
+            dedentValue = position%self.__indentWidth
+            if dedentValue==0:
+                return self.__indentWidth
+            else:
+                return dedentValue
+
+        return 0
+
+
+    # region: event overload ---------------------------------------------------
+
     def resizeEvent(self, event):
         """Code editor is resized
 
@@ -173,6 +237,32 @@ class BSWCodeEditor(QPlainTextEdit):
             blockNumber+=1
 
 
+    def wheelEvent(self, event):
+        """CTRL + wheel os used to zoom in/out font size"""
+        if event.modifiers() == Qt.ControlModifier:
+            delta = event.angleDelta().y()
+            if delta < 0:
+                self.zoomOut()
+            elif delta > 0:
+                self.zoomIn()
+        else:
+            super(BSWCodeEditor, self).wheelEvent(event)
+
+
+    def keyPressEvent(self, event):
+        """Improve editor functionalities with some key bindings"""
+        action = self.shortCut(event.key(), event.modifiers())
+
+        print("keyPressEvent", event.key(), int(event.modifiers()), action)
+        if action is None or event.key() == Qt.Key_Return:
+            super(BSWCodeEditor, self).keyPressEvent(event)
+
+        self.doAction(action)
+
+
+    # endregion: event overload ------------------------------------------------
+
+
     def lineNumberAreaWidth(self):
         """Calculate width for line number area
 
@@ -192,6 +282,264 @@ class BSWCodeEditor(QPlainTextEdit):
 
         # width = (witdh for digit '9') * (number of digits) + 3pixels
         return 3 + self.fontMetrics().width('9') * digits
+
+
+    def doAction(self, action):
+        """Execute given action"""
+        if action is None:
+            return
+        elif action == BSWCodeEditor.KEY_INDENT:
+            self.indent()
+        elif action == BSWCodeEditor.KEY_DEDENT:
+            self.dedent()
+        elif action == BSWCodeEditor.KEY_TOGGLE_COMMENT:
+            self.toggleComment()
+
+
+    def shortCut(self, key, modifiers):
+        """Return action for given key/modifier shortCut
+
+        If nothing is defined, return None
+        """
+        if key in self.__shortCuts:
+            ctrlModifier=(Qt.ControlModifier & modifiers == Qt.ControlModifier)
+            if ctrlModifier in self.__shortCuts[key]:
+                return self.__shortCuts[key][ctrlModifier]
+        return None
+
+
+    def setShortCut(self, key, modifiers, action):
+        """Set action for given key/modifier"""
+        if not action in (BSWCodeEditor.KEY_INDENT,
+                          BSWCodeEditor.KEY_DEDENT,
+                          BSWCodeEditor.KEY_TOGGLE_COMMENT):
+            raise EInvalidValue('Given `action` is not a valid value')
+
+        if modifiers is None:
+            modifiers = BSWCodeEditor.CTRL_KEY_FALSE
+
+        if not key in self.__shortCuts:
+            self.__shortCuts[key]={}
+
+        self.__shortCuts[key][modifiers]=action
+
+
+    def actionShortCut(self, action):
+        """Return shortcut for given action
+
+        If nothing is defined or action doesn't exists, return None
+        If found, Shortcut is returned as a tuple (key, modifiers)
+        """
+        for key in self.__shortCuts:
+            for modifiers in self.__shortCuts[key]:
+                if self.__shortCuts[key][modifiers]==action:
+                    return (key, modifiers)
+        return None
+
+
+    def indentWidth(self):
+        """Return current indentation width"""
+        return self.__indent
+
+
+    def setIndentWidth(self, value):
+        """Set current indentation width
+
+        Must be a value greater than 0
+        """
+        if isinstance(value, int):
+            if value>0:
+                self.__indent=value
+            else:
+                raise EInvalidType("Given `value`must be an integer greater than 0")
+        else:
+            raise EInvalidType("Given `value`must be an integer greater than 0")
+
+
+    def indent(self):
+        """Indent current line or current selection"""
+
+        cursor = self.textCursor()
+
+        selectionStart = cursor.selectionStart()
+        selectionEnd = cursor.selectionEnd()
+
+        if selectionStart == selectionEnd:
+            # No selection: just insert spaces
+            # Note:
+            #   Editor don't add a number of 'indent' spaces, but insert spaces
+            #   to match to column position
+            #
+            #   Example: indent space = 4
+            #            columns: 4, 8, 12, 16, 20, ...
+            #
+            #            x = cursor position
+            #            o = cursor position after tab
+            #            ...|...|...|...|...
+            #            x  o
+            #                x  o
+            #                          xo
+
+            # calculate position relative to start of line
+            cursorSol = self.textCursor()
+            cursorSol.movePosition(QTextCursor.StartOfLine)
+            positionSol = (selectionStart - cursorSol.selectionStart())
+
+            cursor.insertText(" " * self.__calculateIndent(positionSol))
+            return
+
+        # determinate block numbers
+        cursor.setPosition(selectionStart)
+        startBlock = cursor.blockNumber()
+
+        cursor.setPosition(selectionEnd)
+        endBlock = cursor.blockNumber()
+
+        # determinate if last block have to be processed
+        # exemple:
+        #
+        #   +-- Start of selection
+        #   V
+        #   *   Line 1
+        #       Line 2
+        #   *   Line 3
+        #   ^
+        #   +-- End of selection
+        #
+        #   In this case, only the first 2 lines are processed, not the last (nothing selected on last line)
+        #
+        #
+        #   +-- Start of selection
+        #   V
+        #   *   Line 1
+        #       Line 2
+        #       Li*e 3
+        #         ^
+        #         +-- End of selection
+        #
+        #   In this case, the 3 lines are processed
+        #
+        processLastBlock=cursor.selectionStart()
+        cursor.movePosition(QTextCursor.StartOfLine)
+        processLastBlock-=cursor.selectionStart()
+        if processLastBlock>0:
+            processLastBlock=1
+
+
+        cursor.movePosition(QTextCursor.Start)
+        cursor.movePosition(QTextCursor.NextBlock, n=startBlock)
+
+        for blockNumber in range(startBlock, endBlock+processLastBlock):
+            if not self.__isEmptyBlock(blockNumber):
+                # empty lines are not indented
+                nbChar=len(cursor.block().text()) - len(cursor.block().text().lstrip())
+                cursor.movePosition(QTextCursor.StartOfLine)
+                cursor.insertText(" " * self.__calculateIndent(nbChar))
+
+            cursor.movePosition(QTextCursor.NextBlock)
+
+
+    def dedent(self):
+        """Dedent current line or current selection"""
+        cursor = self.textCursor()
+
+        selectionStart = cursor.selectionStart()
+        selectionEnd = cursor.selectionEnd()
+
+        # determinate block numbers
+        cursor.setPosition(selectionStart)
+        startBlock = cursor.blockNumber()
+
+        cursor.setPosition(selectionEnd)
+        endBlock = cursor.blockNumber()
+
+        processLastBlock = cursor.selectionStart()
+        cursor.movePosition(QTextCursor.StartOfLine)
+        processLastBlock-=cursor.selectionStart()
+        if processLastBlock>0 or selectionStart == selectionEnd:
+            processLastBlock=1
+
+        cursor.movePosition(QTextCursor.Start)
+        cursor.movePosition(QTextCursor.NextBlock, n=startBlock)
+
+        for blockNumber in range(startBlock, endBlock + processLastBlock):
+            nbChar=self.__calculateDedent(len(cursor.block().text()) - len(cursor.block().text().lstrip()))
+            if nbChar>0:
+                cursor.movePosition(QTextCursor.StartOfLine)
+                cursor.movePosition(QTextCursor.Right, QTextCursor.KeepAnchor, nbChar)
+                cursor.removeSelectedText()
+
+            cursor.movePosition(QTextCursor.NextBlock)
+
+
+    def toggleComment(self):
+        """Toggle comment for current line or selected lines"""
+        cursor = self.textCursor()
+
+        selectionStart = cursor.selectionStart()
+        selectionEnd = cursor.selectionEnd()
+
+        # determinate block numbers
+        cursor.setPosition(selectionStart)
+        startBlock = cursor.blockNumber()
+
+        cursor.setPosition(selectionEnd)
+        endBlock = cursor.blockNumber()
+
+        processLastBlock = cursor.selectionStart()
+        cursor.movePosition(QTextCursor.StartOfLine)
+        processLastBlock-=cursor.selectionStart()
+        if processLastBlock>0 or selectionStart == selectionEnd:
+            processLastBlock=1
+
+        # True = COMMENT
+        # False = UNCOMMENT
+        hasUncommented=False
+
+        # Work with 2 pass
+        # Pass #1
+        #    Look all blocks
+        #       if at least one block is not commented, then active COMMENT
+        #       if ALL block are commented, then active UNCOMMENT
+        # Pass #2
+        #    Apply COMMENT/UNCOMMENT
+
+        # Pass 1
+        cursor.movePosition(QTextCursor.Start)
+        cursor.movePosition(QTextCursor.NextBlock, n=startBlock)
+
+        for blockNumber in range(startBlock, endBlock + processLastBlock):
+            blockText=cursor.block().text()
+
+            if re.match(r'^\s*#', blockText) is None:
+                hasUncommented = True
+                # dont' need to continue to look content, we know that we have to comment selected text
+                break
+            cursor.movePosition(QTextCursor.NextBlock)
+
+        # Pass 2
+        cursor.movePosition(QTextCursor.Start)
+        cursor.movePosition(QTextCursor.NextBlock, n=startBlock)
+
+        for blockNumber in range(startBlock, endBlock + processLastBlock):
+            blockText=cursor.block().text()
+
+            commentPosition=len(blockText) - len(blockText.lstrip())
+            cursor.movePosition(QTextCursor.StartOfLine)
+            cursor.movePosition(QTextCursor.Right, QTextCursor.MoveAnchor, commentPosition)
+
+            if hasUncommented:
+                # Comment
+                cursor.insertText("# ")
+            else:
+                # Uncomment
+                # Remove hashtag and all following spaces
+                hashtag=re.search('(#+[\s]*)', blockText)
+
+                cursor.movePosition(QTextCursor.Right, QTextCursor.KeepAnchor, len(hashtag.groups()[0]))
+                cursor.removeSelectedText()
+
+            cursor.movePosition(QTextCursor.NextBlock)
 
 
 class BSWLineNumberArea(QWidget):
